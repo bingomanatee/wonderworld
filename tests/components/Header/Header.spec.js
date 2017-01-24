87bfe8bc2cee3bbe20b298f9fa4b2ae491059bb3
David Edelhart <dave@wonderlandlabs.com>
2017-01-23 16:49:10 -0800

    OMG! whats such a big deal! I just wanted to build at last.

--- tests/components/Header/Header.spec.js
+++ tests/components/Header/Header.spec.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import {Header} from 'components/Header/Header';
-import {IndexLink, Link} from 'react-router';
 import {shallow} from 'enzyme';
 
 describe('(Component) Header', () => {
   let _wrapper;
 
   beforeEach(() => {
-    _wrapper = shallow(<Header />);
-  });
-
-  it('Renders a welcome message', () => {
-    const welcome = _wrapper.find('h1');
-    expect(welcome).to.exist;
-    expect(welcome.text()).to.match(/React Redux Starter Kit/);
+    const breadcrumb = [{path: '/homepage', label: 'Home'}];
+    _wrapper = shallow(<Header breadcrumb={breadcrumb} />);
   });
 
   describe('Navigation links...', () => {
     it('Should render a Link to Home route', () => {
-      expect(_wrapper.contains(
-        <IndexLink activeClassName='route--active' to='/'>
-          Home
-        </IndexLink>
-      )).to.be.true;
-    });
-
-    it('Should render a Link to HomeView route', () => {
-      expect(_wrapper.contains(
-        <Link activeClassName='route--active' to='/counter'>
-          Counter
-        </Link>
-      )).to.be.true;
+      const home = _wrapper.find('.header__link');
+      expect(home).to.exist;
+      expect(home.text()).to.match(/<Link \/>/);
     });
   });
 });
