0b57ce24150d4f45ad71f4e1d8f178ef706a36c7
dedelhart <dave.edelhart@sony.com>
2017-02-07 14:53:42 -0800

    proper redux/lock reducer (maybe)

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,175 +1,70 @@
-// There are three possible states for our login
-// process and we need actions for each of them
-export const LOGIN_REQUEST = 'LOGIN_REQUEST'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+import {SERVER_URL} from '../config';
+import Auth0Lock from 'auth0-lock';
 
-function requestLogin(creds) {
+export const lock = new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, {
+  auth: {
+    redirectUrl: `${SERVER_URL}/login`,
+    responseType: 'token'
+  }
+});
+
+export function getIdToken() {
+  // First, check if there is already a JWT in local storage
+  var idToken = localStorage.getItem('id_token');
+  var authHash = this.lock.parseHash(window.location.hash);
+  // If there is no JWT in local storage and there is one in the URL hash,
+  // save it in local storage
+  if (!idToken && authHash) {
+    if (authHash.id_token) {
+      idToken = authHash.id_token
+      localStorage.setItem('id_token', authHash.id_token);
+    }
+    if (authHash.error) {
+      // Handle any error conditions
+      console.log("Error signing in", authHash);
+    }
+  }
+  return idToken;
+}
+
+const LOAD_USER = 'LOAD_USER';
+const SET_USER = 'SET_USER';
+
+export function setUser(profile) => {
   return {
-    type: LOGIN_REQUEST,
-    isFetching: true,
-    isAuthenticated: false,
-    creds
+    type: SET_USER,
+    payoad: profile
   }
 }
 
-function receiveLogin(user) {
-  return {
-    type: LOGIN_SUCCESS,
-    isFetching: false,
-    isAuthenticated: true,
-    id_token: user.id_token
+export const loadUser = () => {
+  return (dispatch) => {
+    let token = getIdToken();
+    if (!token) {
+      dispatch(setUser(false));
+    } else {
+      lock.getProfile(token, (err, profile) => {
+        dispatch.setUser(profile || false);
+      });
+    }
   }
-}
+};
 
-function loginError(message) {
-  return {
-    type: LOGIN_FAILURE,
-    isFetching: false,
-    isAuthenticated: false,
-    message
-  }
-}
+// ------------------------------------
+// Action Handlers
+// ------------------------------------
+const ACTION_HANDLERS = {
+  [SET_USER]: (state, action) => Object.assign({}, state, {profile: action.payload})
+};
 
-// Three possible states for our logout process as well.
-// Since we are using JWTs, we just need to remove the token
-// from localStorage. These actions are more useful if we
-// were calling the API to log the user out
-export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
-export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
-
-function requestLogout() {
-  return {
-    type: LOGOUT_REQUEST,
-    isFetching: true,
-    isAuthenticated: true
-  }
-}
-
-function receiveLogout() {
-  return {
-    type: LOGOUT_SUCCESS,
-    isFetching: false,
-    isAuthenticated: false
-  }
-}
-
-// Calls the API to get a token and
-// dispatches actions along the way
-export function loginUser(creds) {
-
-  let config = {
-    method: 'POST',
-    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
-    body: `username=${creds.username}&password=${creds.password}`
-  }
-
-  return dispatch => {
-    // We dispatch requestLogin to kickoff the call to the API
-    dispatch(requestLogin(creds))
-    return fetch('http://localhost:3001/sessions/create', config)
-      .then(response =>
-        response.json()
-          .then(user => ({ user, response }))
-      ).then(({ user, response }) =>  {
-        if (!response.ok) {
-          // If there was a problem, we want to
-          // dispatch the error condition
-          dispatch(loginError(user.message))
-          return Promise.reject(user)
-        }
-        else {
-          // If login was successful, set the token in local storage
-          localStorage.setItem('id_token', user.id_token)
-
-          // Dispatch the success action
-          dispatch(receiveLogin(user))
-        }
-      }).catch(err => console.log("Error: ", err))
-  }
-}
-
-// Logs the user out
-export function logoutUser() {
-  return dispatch => {
-    dispatch(requestLogout());
-    localStorage.removeItem('id_token');
-    dispatch(receiveLogout())
-  }
-}
-
-// The auth reducer. The starting state sets authentication
-// based on a token being in local storage. In a real app,
-// we would also want a util to check if the token is expired.
-export default function auth(state = {
-  isFetching: false,
-  isAuthenticated: localStorage.getItem('id_token') ? true : false
-}, action) {
-  switch (action.type) {
-    case LOGIN_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false,
-        user: action.creds
-      })
-    case LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: true,
-        errorMessage: ''
-      })
-    case LOGIN_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: false,
-        errorMessage: action.message
-      })
-    case LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false
-      })
-    default:
-      return state
-  }
-}
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = [];
-// The auth reducer. The starting state sets authentication
-// based on a token being in local storage. In a real app,
-// we would also want a util to check if the token is expired.
-export default function auth(state = {
-  isFetching: false,
-  isAuthenticated: localStorage.getItem('id_token') ? true : false
-}, action) {
-  switch (action.type) {
-    case LOGIN_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false,
-        user: action.creds
-      })
-    case LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: true,
-        errorMessage: ''
-      })
-    case LOGIN_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: false,
-        errorMessage: action.message
-      })
-    case LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false
-      })
-    default:
-      return state
-  }
+const initialState = {
+  profile: {}
+};
+export default function userReducer (state = initialState, action) {
+  const handler = ACTION_HANDLERS[action.type];
+
+  return handler ? handler(state, action) : state;
 }
